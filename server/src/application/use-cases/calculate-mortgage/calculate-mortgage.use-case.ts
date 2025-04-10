2b6a3e819abf781aeadaca44c24a1461c4530214
Wallace de Freitas <wallace.freitas.b@gmail.com>
2025-04-10 11:43:53 -0300

    chore(backend): remove console.log

--- server/src/application/use-cases/calculate-mortgage/calculate-mortgage.use-case.ts
+++ server/src/application/use-cases/calculate-mortgage/calculate-mortgage.use-case.ts
@@ -40,19 +40,11 @@ export class CalculateMortgageUseCase {
       calculatePeriodChoosed.calculate(interestRate, amortizationPeriod);
 
     const powerTerm = Math.pow(1 + periodicInterestRate, numberOfPayments); // Step 1: Calculate (1 + r)^n
-    console.log("periodicInterestRate", periodicInterestRate);
-    console.log("numberOfPayments", numberOfPayments);
-    console.log("powerTerm", powerTerm);
     const numerator = periodicInterestRate * powerTerm; // Step 2: Calculate r * (1 + r)^n
     const denominator = powerTerm - 1; // Step 3: Calculate (1 + r)^n - 1
-    console.log("numerator", numerator);
-    console.log("denominator", denominator);
     const rateFactors = Number((numerator / denominator).toFixed(8)); // Step 4: Calculate rate factors with fixed precision
-    console.log("rateFactors", rateFactors);
     const payment = totalLoanAmount * rateFactors; // Step 5: Calculate payment
-    console.log("payment", payment);
     const basePayment = Math.round(payment * 100) / 100; // Step 6: Round to 2 decimal places using Math.round
-    console.log("basePayment", basePayment);
 
     return Number(basePayment.toFixed(2));
   }
